5afadb232b311ab32ba3ce486af4aeb8cd346d21
nikolinatomasevic <nikolina.tomasevic2000@gmail.com>
2022-06-19 15:45:32 +0200

    Koriscenjem useState i useEffect React Hooks i axios GET zahteva, dobijeni svi polaznici auto škola iz baze

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,4 +1,23 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
 function Home(){
+
+
+    const [polaznici, setPolaznici] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/polaznik').then(response => {
+
+            setPolaznici(response.data.data);
+
+        });
+
+    }, []);
+
+
+
     return (
         <div>
 
